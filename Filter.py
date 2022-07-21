e495f9012a9eea50e43d21ff69e89304c97144fa
SinPalo <72827563+SinPalo@users.noreply.github.com>
2022-07-21 10:26:05 -0500

    Add files via upload
    Actualizo el filtro con notebook más limpio y el filtro que incluye los diccionarios

--- Filter.py
+++ Filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 import pandas as pd
 from statsmodels.tsa.stattools import coint
 import matplotlib.pyplot as plt
@@ -7,10 +8,11 @@ import datetime
 import seaborn as sns
 from Topological_Embedding import Tembedding
 from Funciones import varInfo, correlation_distance
+from Dicts import Dicts
 
 
 
-class ETF_Filter:
+class EtfFilter:
 
     def __init__(self, etfs, tickers, start, end):
 
@@ -22,7 +24,7 @@ class ETF_Filter:
         
         self.etfs = sorted(etfs)
         
-        self.df = yf.download(self.tickers + self.etfs, self.start, self.end)['Adj Close'].dropna()
+        self.df = yf.download(self.tickers + self.etfs, self.start, self.end)['Close'].dropna()
         
         self.etf_filtration = self.df[self.etfs].copy()
 
@@ -48,8 +50,7 @@ class ETF_Filter:
 
 
     def correlationTest(self, tol = 0.9):
-         
-
+        
         for pair in self.pairs.copy():
                         
             resultado = np.corrcoef(self.etf_filtration[pair[0]],self.ticker_filtration[pair[1]])[0,1]
@@ -165,14 +166,12 @@ class PairsFilter:
 
 
     def correlationTest(self, tol = 0.9):
-         
-
+       
         for pair in self.pairs.copy():
+            resultado = np.corrcoef(self.data_filtration[pair[0]],self.data_filtration[pair[1]])[0,1]
+            if resultado < tol:
 
-                resultado = np.corrcoef(self.data_filtration[pair[0]],self.data_filtration[pair[1]])[0,1]
-                if resultado < tol:
-
-                    self.pairs.remove(pair)
+                self.pairs.remove(pair)
             
         self.__filter_data()
 
@@ -239,13 +238,6 @@ class PairsFilter:
                 unique_tickers.append(pair[1])
         
         self.data_filtration = self.data_filtration[unique_tickers]
-
-    def get_all_filters(self, tol_varinfo = 1.0, bins_varinfo = 30, tol_Temb = 80, tol_corr=0.8, tol_coint=0.1):
-        self.correlationTest(tol = tol_corr)
-        self.variationalInformationTest(tol = tol_varinfo, bins = bins_varinfo)
-        self.TopologicalEmbedding(tol=tol_Temb)
-        self.cointegration(tol=tol_coint)
-
         
     
     def plot_correlation(self, method = 'pearson', *args):
@@ -269,3 +261,146 @@ class PairsFilter:
         plt.figure(figsize=(25,25))
         sns.heatmap(metrica, cmap = sns.cm.rocket_r)
         
+        
+class PairsFilter2:
+
+    def __init__(self, stock_type, sector="", number=1000, **kwargs):
+
+        
+        dicts = Dicts()
+        stocks_dict = dicts.stocks_dict()
+        etf_list = dicts.etf_dict()
+        forex_list = dicts.forex_dict()
+        crypto_list = dicts.crypto_dict()
+        diccionarioStocks = { 'Crypto':crypto_list, 'Forex': forex_list, 'Stock': stocks_dict, "Etf": etf_list}
+        if stock_type != 'Stock':
+            if number < len(diccionarioStocks[stock_type]):
+                self.tickers = np.random.permutation(diccionarioStocks[stock_type])[:number].tolist()
+            else:
+                print('El numero de tickers que escogiste es mayor al que tenemos, así que te muestro todos')
+                self.tickers = diccionarioStocks[stock_type]
+
+        else: 
+            if number < len(diccionarioStocks[stock_type]):
+                self.tickers = (np.random.permutation(diccionarioStocks[stock_type][sector])[:number]).tolist()
+            else:
+                print('El numero de tickers que escogiste es mayor al que tenemos, así que te muestro todos')
+                self.tickers = diccionarioStocks[stock_type][sector]
+
+
+        #tickers=diccionarioStocks[stock_type][sector]
+        self.df = yf.download(self.tickers, **kwargs)['Close'].dropna()
+        self.data_filtration = self.df.copy()
+        self.pairs = []
+        for i in range(len(self.tickers)):
+            for j in range(i+1,len(self.tickers)):
+                self.pairs.append((self.tickers[i],self.tickers[j]))
+    
+    def TopologicalEmbedding(self, tol=100, lagtime=1):
+        
+        
+        topo = Tembedding(self.data_filtration)
+        
+        for pair in self.pairs.copy():
+            resultado = topo.hausdorff(pair[0], pair[1])
+            if resultado > tol:
+                self.pairs.remove(pair)
+            
+        self.__filter_data()
+
+
+    def correlationTest(self, tol = 0.9):
+       
+        for pair in self.pairs.copy():
+            resultado = np.corrcoef(self.data_filtration[pair[0]],self.data_filtration[pair[1]])[0,1]
+            if resultado < tol:
+
+                self.pairs.remove(pair)
+            
+        self.__filter_data()
+
+
+
+    def cointegration(self, tol=0.1):
+
+        for pair in self.pairs.copy():
+
+                resultado = coint(self.data_filtration[pair[0]], self.data_filtration[pair[1]])[1]
+
+                if resultado > tol:
+
+                    self.pairs.remove(pair)
+                    
+        self.__filter_data()
+        
+    
+    def distance_correlationTest(self, tol=0.3):
+        
+        for pair in self.pairs.copy():
+            
+            resultado = correlation_distance(self.data_filtration[pair[0]], self.data_filtration[pair[1]])
+            
+            if resultado > tol:
+                self.pairs.remove(pair)
+                
+        self.__filter_data()
+   
+                
+
+
+    def variationalInformationTest(self, bins = 50, tol = 3.0):
+    
+        for pair in self.pairs.copy():
+
+            resultado = varInfo(self.data_filtration[pair[0]],self.data_filtration[pair[1]], bins = bins)
+
+            if resultado > tol:
+
+                self.pairs.remove(pair)
+                
+        self.__filter_data()
+
+    
+    def clear_filter(self):
+
+        self.pairs = []
+        self.data_filtration = self.df.copy()
+
+        for i in range(len(self.tickers)):
+            for j in range(i+1,len(self.tickers)):
+                self.pairs.append((self.tickers[i],self.tickers[j]))
+            
+  
+    def __filter_data(self):
+    
+        unique_tickers = []
+        
+        for pair in self.pairs:
+            if pair[0] not in unique_tickers:
+                unique_tickers.append(pair[0])
+            if pair[1] not in unique_tickers:
+                unique_tickers.append(pair[1])
+        
+        self.data_filtration = self.data_filtration[unique_tickers]
+        
+    
+    def plot_correlation(self, method = 'pearson', *args):
+        plt.figure(figsize=(25,25))
+        sns.heatmap(self.data_filtration.corr(method = method), cmap = sns.cm.rocket, *args)
+        
+    def plot_variational_information(self, bins= 50, *args):
+        varinfo_matrix = np.zeros([self.data_filtration.shape[1], self.data_filtration.shape[1]])
+        for i in range(self.data_filtration.shape[1]):
+            for j in range(self.data_filtration.shape[1]):
+                varinfo_matrix[i,j] = varInfo(self.data_filtration.iloc[:,i], self.data_filtration.iloc[:,j], bins = 50)
+        varinfo_matrix = pd.DataFrame(varinfo_matrix, columns=self.data_filtration.columns)
+        
+        plt.figure(figsize=(25,25))
+        sns.heatmap(varinfo_matrix, cmap = sns.cm.rocket, xticklabels=self.tickers, yticklabels=self.tickers, *args)
+        
+    def plot_correlationDistance(self, method = 'pearson', *args):
+        
+        cor = self.data_filtration.corr(method='pearson')
+        metrica = np.sqrt(0.5*(1-cor))
+        plt.figure(figsize=(25,25))
+        sns.heatmap(metrica, cmap = sns.cm.rocket_r)
